a4f68f011660367a519b54703650e4afa909bfe3
Josh Thompson <thompsonjoshd@gmail.com>
2023-07-28 13:38:30 -0600

    add basic user test and validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,5 +9,24 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test 'requires a valid email' do
+    @user = User.new(name: "Josh", email: "")
+    assert_not @user.valid?
+
+    @user.email = "example@example.com"
+    assert @user.valid?
+  end
+
+  test 'requires a unique email' do
+    @existing_user = User.create(
+      name: "Josh", email: "example@example.com"
+    )
+    assert @existing_user.persisted?
+    
+    @dup_email_user = User.new(name: "Josh", email: "example@example.com")
+    
+    assert_not @dup_email_user.valid?
+  end
+
   
 end
