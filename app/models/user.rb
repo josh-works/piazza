0d0028912f0b7a8940f9d040feb4cfce592ce862
Josh Thompson <thompsonjoshd@gmail.com>
2023-07-28 13:41:30 -0600

    add associations to User and Organization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    has_many :memberships, dependent: :destroy
+    has_many :organizations, through: :memberships
+
     validates :name, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
                       uniqueness: { case_sensitive: false }
